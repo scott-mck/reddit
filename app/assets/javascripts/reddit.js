fd45410436f24f6971cc38bd1b8a5f5ad0796d08
Scott McKenzie <slammayjammay@gmail.com>
2015-08-28 18:50:20 -0700

    Show sponsored post and trending subreddits only on root

--- app/assets/javascripts/reddit.js
+++ app/assets/javascripts/reddit.js
@@ -4,36 +4,38 @@ window.Reddit = {
   Views: {},
   Routers: {},
   initialize: function() {
-    var sponsoredPost = new Reddit.Collections.Posts();
-    sponsoredPost.fetch({
-      url: 'https://www.reddit.com/rising.json',
-      dataType: 'jsonp',
-      jsonp: 'jsonp',
-      success: function () {
-        view = new Reddit.Views.PostsIndexItem({
-          model: sponsoredPost.first(),
-        });
-        $('#sponsored').prepend(view.render().$el.addClass('sponsored'));
-      }
-    });
+    if (window.location.pathname === '/') {
+      var sponsoredPost = new Reddit.Collections.Posts();
+      sponsoredPost.fetch({
+        url: 'https://www.reddit.com/rising.json',
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        success: function () {
+          view = new Reddit.Views.PostsIndexItem({
+            model: sponsoredPost.first(),
+          });
+          $('#sponsored').prepend(view.render().$el.addClass('sponsored'));
+        }
+      });
 
-    var trending = $.ajax({
-      url: 'https://reddit.com/r/trendingsubreddits.json',
-      dataType: 'jsonp',
-      jsonp: 'jsonp',
-      success: function () {
-        var trendingPost = trending.responseJSON.data.children[0].data;
-        var regEx = /\/r\/\w*/g;
-        var subs = trendingPost.title.match(regEx);
+      var trending = $.ajax({
+        url: 'https://reddit.com/r/trendingsubreddits.json',
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        success: function () {
+          var trendingPost = trending.responseJSON.data.children[0].data;
+          var regEx = /\/r\/\w*/g;
+          var subs = trendingPost.title.match(regEx);
 
-        var view = new Reddit.Views.TrendingSubreddits({
-          subs: subs,
-          numComments: trendingPost.num_comments,
-          permalink: trendingPost.permalink
-        });
-        $('#trending-subreddits').append(view.render().$el);
-      }
-    });
+          var view = new Reddit.Views.TrendingSubreddits({
+            subs: subs,
+            numComments: trendingPost.num_comments,
+            permalink: trendingPost.permalink
+          });
+          $('#trending-subreddits').append(view.render().$el);
+        }
+      });
+    }
 
     var posts = new Reddit.Collections.Posts();
     posts.fetch({
