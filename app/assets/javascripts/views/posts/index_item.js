825b4904f44cfc8d1b97744a26cb39c116395126
Scott McKenzie <slammayjammay@gmail.com>
2015-09-02 22:23:43 -0700

    Fix rank width problems

--- app/assets/javascripts/views/posts/index_item.js
+++ app/assets/javascripts/views/posts/index_item.js
@@ -1,7 +1,6 @@
 // domain links to real reddit /domain
 // link title links to real reddit post
 // user links to actual user's page
-// TODO: vote numbers mess lots of things up
 // TODO: no paddingLeft for thumbnails === ''
 
 Reddit.Views.PostsIndexItem = Backbone.View.extend({
