0a25d0162b332cb409c1ab3119609177f5a13625
Scott McKenzie <slammayjammay@gmail.com>
2015-09-12 16:18:48 -0700

    Next button fetches correct content

--- app/assets/javascripts/views/view_more.js
+++ app/assets/javascripts/views/view_more.js
@@ -2,9 +2,22 @@ Reddit.Views.ViewMore = Backbone.View.extend({
   template: JST['view_more'],
   className: 'view-more',
 
-  render: function () {
-    var content = this.template({
+  initialize: function (options) {
+    this.after = options.after;
+    this.before = options.before;
+  },
 
+  render: function () {
+    var searchQuery;
+    if (window.location.search === '') {
+      searchQuery = '?';
+    } else {
+      searchQuery = window.location.search + '&';
+    }
+    var content = this.template({
+      currentUrl: window.location.pathname + searchQuery,
+      after: this.after,
+      before: this.before
     });
     this.$el.html(content);
     return this;
