bd4dd736714e4c57d72379d1aa4514c087fc15be
Scott McKenzie <slammayjammay@gmail.com>
2015-09-12 19:54:39 -0700

    Next and prev buttons fetch and show ranks correctly

--- app/assets/javascripts/reddit.js
+++ app/assets/javascripts/reddit.js
@@ -52,16 +52,34 @@ window.Reddit = {
       dataType: 'jsonp',
       jsonp: 'jsonp',
       success: function (collection, resp) {
-        var count = window.location.search.match(/count=(\d+)/) || 0;
+        // TODO: there HAS to be a better way to do this
+        var count = window.location.search.match(/count=(\d+)/);
         if (count) {
-          count = count[1];
+          count = parseInt(count[count.length - 1]);
         }
-        count = parseInt(count);
-        
+        var dir = window.location.search.match(/(after|before)=\w+/) || [''];
+
+        var countAfter, countBefore;
+        var getIndex = function (n) {
+          return count + n + 1;
+        }
+        if (dir[dir.length - 1] === 'after') {
+          countAfter = count + 25;
+          countBefore = count + 1;
+        } else if (dir[dir.length - 1] === 'before') {
+          countAfter = count - 1;
+          countBefore = count - 25;
+          getIndex = function (n) {
+            return count - (25 - n);
+          };
+        } else {
+          countAfter = 25;
+        }
+
         posts.each(function (post) {
            view = new Reddit.Views.PostsIndexItem({
             model: post,
-            index: count + posts.indexOf(post) + 1
+            index: getIndex(posts.indexOf(post))
           });
           var clearDiv = $("<div style='clear: left;'>");
           $('#posts').append(view.render().$el).append(clearDiv);
@@ -69,7 +87,9 @@ window.Reddit = {
 
         var view = new Reddit.Views.ViewMore({
           after: resp.data.after,
-          before: resp.data.before
+          before: resp.data.before,
+          countAfter: countAfter,
+          countBefore: countBefore
         });
         $('#main').append(view.render().$el);
       }
--- app/assets/javascripts/views/view_more.js
+++ app/assets/javascripts/views/view_more.js
@@ -5,16 +5,18 @@ Reddit.Views.ViewMore = Backbone.View.extend({
   initialize: function (options) {
     this.after = options.after;
     this.before = options.before;
+    this.countAfter = options.countAfter;
+    this.countBefore = options.countBefore;
   },
 
   render: function () {
     var query = window.location.search;
-    var count = query.match(/(&*count=\d+)/) || ['', ''];
-    var after = query.match(/(&*after=\w+)/) || ['', ''];
-    var before = query.match(/(&*before=\w+)/) || ['', ''];
-    query = query.replace(count[1], '');
-    query = query.replace(after[1], '');
-    query = query.replace(before[1], '');
+    var count = query.match(/(&*count=\d+)/) || [''];
+    var after = query.match(/(&*after=\w+)/) || [''];
+    var before = query.match(/(&*before=\w+)/) || [''];
+    query = query.replace(count[count.length - 1], '');
+    query = query.replace(after[after.length - 1], '');
+    query = query.replace(before[before.length - 1], '');
 
     if (query === '' || query === '?') {
       query = '?';
@@ -24,7 +26,9 @@ Reddit.Views.ViewMore = Backbone.View.extend({
     var content = this.template({
       currentUrl: window.location.pathname + query,
       after: this.after,
-      before: this.before
+      before: this.before,
+      countAfter: 'count=' + this.countAfter,
+      countBefore: 'count=' + this.countBefore
     });
     this.$el.html(content);
     return this;
