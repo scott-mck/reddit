cf3a25a9560b0c1156dde7a205615182d389955d
Scott McKenzie <slammayjammay@gmail.com>
2015-09-19 01:25:13 -0700

    Remove unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'r#index'
   post '/' => 'r#search', as: 'front'
-  post 'r/(*subreddit)/' => 'r#search', as: 'subreddits'
   get 'r/(*subreddit)/(*sort)' => 'r#index'
   get '(*sort)' => 'r#index'
 end
