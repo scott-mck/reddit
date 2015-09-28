a09acf5fdf05c534c00709cfc38593dc9f556b66
Scott McKenzie <slammayjammay@gmail.com>
2015-09-27 22:55:54 -0700

    Move reddit html method to r controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def get_reddit_html # TODO: make this async somehow (maybe?)
-    data = {}
-    gold = Nokogiri::HTML(open('https://www.reddit.com')).css('.goldvertisement')
-    data[:gold_progress] = gold.css('.progress').css('p')[0].text
-    data[:prev_gold_progress] = gold.css('.history').css('.progress').css('p')[0].text
-    data[:time_to_midnight] = /\(.*\)/.match gold.css('.aside').css('p').text
-    data
-  end
 end
--- app/controllers/r_controller.rb
+++ app/controllers/r_controller.rb
@@ -1,16 +1,26 @@
 # TODO: Fetch correct sponsored posts
 
 class RController < ApplicationController
+  before_action :get_reddit_html
+
   def index
     @show_time_sort = true if %w(controversial top).include? params[:sort]
     @t = params[:t] || 'day'
 
-    @data = get_reddit_html
     @subreddit = params[:subreddit] ? "/r/#{params[:subreddit]}/" : "/"
     @sort = params[:sort]
   end
 
+  def get_reddit_html # TODO: make this async somehow (maybe?)
+    @data = {}
+    gold = Nokogiri::HTML(open('https://www.reddit.com')).css('.goldvertisement')
+    @data[:gold_progress] = gold.css('.progress').css('p')[0].text
+    @data[:prev_gold_progress] = gold.css('.history').css('.progress').css('p')[0].text
+    @data[:time_to_midnight] = /\(.*\)/.match gold.css('.aside').css('p').text
+    @data
+  end
+
   def show
-    
+
   end
 end
