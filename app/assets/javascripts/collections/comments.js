3328bb7265a53e38ab57dcb902234b6cdd71906c
Scott McKenzie <slammayjammay@gmail.com>
2015-09-27 23:15:03 -0700

    Fix parse

--- app/assets/javascripts/collections/comments.js
+++ app/assets/javascripts/collections/comments.js
@@ -2,9 +2,9 @@ Reddit.Collections.Comments = Backbone.Collection.extend({
   model: Reddit.Models.Comment,
 
   parse: function (res) {
-    if (res.data.children) {
-      if (res.data.children[0].kind === "t1") {
-        return res.data.children; // fetch only comments
+    if (res[1].data.children) {
+      if (res[1].data.children[0].kind === "t1") {
+        return res[1].data.children; // fetch only comments
       }
     }
   }
