cdeb3fdef5091f46a0d66fe5247bdd04e4089a5e
Scott McKenzie <slammayjammay@gmail.com>
2015-09-27 22:50:38 -0700

    Ensure fetching retrieves only posts (kind t3)

--- app/assets/javascripts/collections/posts.js
+++ app/assets/javascripts/collections/posts.js
@@ -2,6 +2,10 @@ Reddit.Collections.Posts = Backbone.Collection.extend({
   model: Reddit.Models.Post,
 
   parse: function (res) {
-    return res.data.children;
+    if (res.data) {
+      if (res.data.children[0].kind === "t3") {
+        return res.data.children; // fetch only posts
+      }
+    }
   }
 });
