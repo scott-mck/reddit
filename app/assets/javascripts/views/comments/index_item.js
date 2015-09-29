f290dce5856c960a9d126eff242487fbe5db2e17
Scott McKenzie <slammayjammay@gmail.com>
2015-09-28 22:06:30 -0700

    Show comment replies recursively

--- app/assets/javascripts/views/comments/index_item.js
+++ app/assets/javascripts/views/comments/index_item.js
@@ -1,14 +1,32 @@
-Reddit.Views.CommentsIndexItem = Backbone.View.extend({
+Reddit.Views.CommentsIndexItem = Backbone.CompositeView.extend({
   template: JST['comments/index_item'],
   className: 'comments-index-item',
+
+  addReplies: function () {
+    if (this.model.get('data').replies && this.model.get('data').replies !== "") {
+
+      this.model.get('data').replies.data.children.forEach(function (replyData) {
+        var reply = new Reddit.Models.Comment(replyData);
+        var commentView = new Reddit.Views.CommentsIndexItem({
+          model: reply
+        });
+
+        this.addSubview('.' + this.model.get('data').name, commentView);
+      }.bind(this));
+
+    }
+  },
 
   render: function () {
     var content = this.template({
       author: this.model.get('data').author,
       body: this.model.get('data').body,
-      score: this.model.get('data').score
+      score: this.model.get('data').score,
+      name: this.model.get('data').name
     });
     this.$el.html(content);
+
+    this.addReplies();
     return this;
   }
 });
